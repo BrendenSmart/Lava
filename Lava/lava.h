5a9256c21a09c0110e26b44976b4f74c085a222d
BrendenSmart <brenden.smart24@gmail.com>
2022-09-04 17:23:14 -0500

    Add project files.

---
+++ Lava/lava.h
@@ -0,0 +1,29 @@
+namespace Lava {
+	namespace Graphics {
+
+
+	}
+
+	namespace Internal {
+		
+		namespace LavaVk {
+
+		}
+
+		namespace LavaML {
+
+		}
+
+		namespace LavaD2D {
+
+		}
+
+		namespace LavaGL {
+
+		}
+
+		namespace LavaGLES {
+
+		}
+	}
+}
